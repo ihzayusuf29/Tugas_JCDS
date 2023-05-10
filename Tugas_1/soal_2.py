8a9e751b074efdfe39cc6083ea2b4a01764a4e3b
ihzayusuf29 <62004880+ihzayusuf29@users.noreply.github.com>
2023-05-10 20:41:41 +0700

    Create soal_2.py

---
+++ Tugas_1/soal_2.py
@@ -0,0 +1,5 @@
+if __name__== '__main__':
+    #Question_2
+    angka = float(input("silahkan masukan angka berapapun: "))
+    angka_kuadrat = angka**2
+    print (f"kuadrat dari {angka} = {angka_kuadrat}")
