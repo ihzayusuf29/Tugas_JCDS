f34f07c1d7ca70a398dfee551df5e3e37a0728af
ihzayusuf29 <62004880+ihzayusuf29@users.noreply.github.com>
2023-05-10 20:44:20 +0700

    Create soal_4.py

---
+++ Tugas_1/soal_4.py
@@ -0,0 +1,10 @@
+if __name__== '__main__':
+    #question_4
+    umur_andi = (4/14)*49
+    umur_budi = (10/14*49)
+
+    umur_andi_2tahun_lagi = (umur_andi + 2)
+    umur_budi_2tahun_lagi = (umur_budi + 2)
+
+    print (umur_andi_2tahun_lagi)
+    print (umur_budi_2tahun_lagi)
